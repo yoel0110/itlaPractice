20de63e836ff933fe03c9d29f71abca997b969f5
Yoel 4Devs <Yoellebron35@gmail.com>
2026-01-31 20:49:15 -0400

    Use of constructor

---
+++ oop/oopFundamental/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace oopFundamental
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var corola = new Car();
+
+            corola.PrintState();
+            corola.SpeedUp(240);
+            corola.PrintState();
+
+            Car test = new("Juan", 140);
+
+            test.PrintState();
+            test.SpeedUp(10);
+            test.PrintState();
+
+        }
+    }
+}
---
+++ oop/oopFundamental/class/Car.cs
@@ -0,0 +1,20 @@
+﻿namespace oopFundamental
+{
+    class Car
+    {
+        public string petName = String.Empty;
+        public int currSpeed;
+
+        public Car(){}
+        public Car(string petName, int currSpeed)
+        {
+            this.petName = petName;
+            this.currSpeed = currSpeed;
+        }
+
+
+        public void PrintState() => Console.WriteLine($"{petName} Is going {currSpeed} MPH.");
+
+        public void SpeedUp(int delta) => currSpeed += delta; 
+    }
+}
